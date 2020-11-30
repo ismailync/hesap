919c44240c017a1ac01fa39a2f28a7d6a8b306ac
ismailync <ismailyonca2001@gmail.com>
2020-11-30 22:53:16 +0300

    .

---
+++ hesap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hesap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+       
+        public class Dikdortgen
+        {
+            int b;
+            int h;
+        }
+
+        public static void alanhesapla()
+        {
+
+        }
+        public static void cevrehesapla()
+        {
+
+        }
+
+    }
+}
